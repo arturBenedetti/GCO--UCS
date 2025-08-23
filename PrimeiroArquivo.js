0ac4e6123bac719ea816a2d047bdded3ea2e5a6a
Artur Benedetti <atbbenedetti@gmail.com>
2025-08-23 11:10:25 -0300

    outra alteração mas sem dar merge anteriormente

--- PrimeiroArquivo.js
+++ PrimeiroArquivo.js
@@ -1,3 +1,4 @@
 console.log('Primeiro arquivo!');
 console.log('Alteração em outra branch');
 console.log('Criação na branch dev e depois merge para a main');
+console.log('Alteração na branch dev sem dar merge pra main no commit anterior');
